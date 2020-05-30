6b2237bccd7ebdb3674a21d78c38efeeae2ae48c
reynard <reynard93@gmail.com>
2020-05-30 13:25:08 +0800

    style game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {StyledCharacter, StyledGame, StyledScore, Styledtimer} from "../styled/Game";
+import {Strong} from "../styled/Random";
 
 function Game(props) {
 	return (
-			<div></div>
+			<StyledGame>
+				<StyledScore>Score: <Strong>0</Strong></StyledScore>
+				<StyledCharacter>A</StyledCharacter>
+				<Styledtimer>Time: <Strong>00: 000</Strong></Styledtimer>
+			</StyledGame>
 	);
 }
 
--- src/styled/Container.js
+++ src/styled/Container.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   padding: 20px;
-  margin: 0 auto;
-  margin-top: 20px;
+  margin: 20px auto 0;
   max-width: 800px;
 `;
