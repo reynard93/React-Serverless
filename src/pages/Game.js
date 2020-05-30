d96bd89bf0ecf66941024e017f18739836438336
reynard <reynard93@gmail.com>
2020-05-30 17:25:14 +0800

    added game timer

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,12 +1,23 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import {StyledCharacter, StyledGame, StyledScore, Styledtimer} from "../styled/Game";
 import {Strong} from "../styled/Random";
+import {questions as QUESTIONS} from "./questions";
 
 function Game({history}) {
 	const [score, setScore] = useState(0);
-	const MAX_SECONDS = 5;
+	const questions = Object.entries(QUESTIONS);
+	const questionsLength = 38;
+	const [currentQuestion, setCurrentQuestion] = useState(['','']);
+	const MAX_SECONDS = 100;
 	const [ms, setMs] = useState(0);
 	const [seconds, setSeconds] = useState(MAX_SECONDS);
+
+	const setRandomQuestion = useCallback(() => {
+		const randomQuestionIndex = Math.floor(Math.random() * questionsLength);
+		console.log(questions, 'hjave questiosn ehre??')
+		console.log(randomQuestionIndex, 'what my indedx')
+		setCurrentQuestion(questions[randomQuestionIndex]);
+	}, [questions])
 
 	const updateTime = useCallback((startTime) => {
 		const endTime = new Date();
@@ -16,7 +27,6 @@ function Game({history}) {
 		// 00000 - first 2 are seconds, last 3 are the ms that have passed
 		const updatedSeconds = MAX_SECONDS - parseInt(formattedMSString.substring(0, 2)) - 1;
 		const updatedMs = 1000 - parseInt(formattedMSString.substring(formattedMSString.length - 3));
-		console.log(addLeadingZeros(updatedSeconds, 2), addLeadingZeros(updatedMs, 3))
 		setSeconds(addLeadingZeros(updatedSeconds, 2))
 		setMs(addLeadingZeros(updatedMs, 3))
 	}, [])
@@ -32,21 +42,36 @@ function Game({history}) {
 
 		useEffect(() => {
 			const currentTime = new Date();
+			const questionInterval = setInterval(() => setRandomQuestion(), 2000); // to be adjusted frm ui
 			const interval = setInterval(() => updateTime(currentTime), 1);
-			return () => clearInterval(interval);
-		}, [updateTime]);
+			return () => {
+				clearInterval(questionInterval);
+				clearInterval(interval);
+			}
+		}, [setRandomQuestion, updateTime]);
 
 		useEffect(() => {
 			if (seconds <= -1) {
 				console.log('/gameOver');
 				history.push('/gameOver');
 			}
-		}, [seconds, ms, history])
+		}, [seconds, ms, history, questions])
+
+	const keyUpHandler = (e) => {
+			console.log(e.key);
+	}
+
+	useEffect(() => {
+		document.addEventListener('keyup', keyUpHandler)
+		return () => {
+			document.removeEventListener('keyup', keyUpHandler);
+		}
+	}, [])
 
 	return (
 			<StyledGame>
 				<StyledScore>Score: <Strong>{score}</Strong></StyledScore>
-				<StyledCharacter>A</StyledCharacter>
+				<StyledCharacter>{currentQuestion[1]}</StyledCharacter>
 				<Styledtimer>Time: <Strong>{seconds}: {ms}</Strong></Styledtimer>
 			</StyledGame>
 	);
